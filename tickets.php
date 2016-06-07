980adf4677770254f097784e28e760501de58227
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-06-07 18:49:34 +0200

    Logic for handling payment requests. Still needs work

--- tickets.php
+++ tickets.php
@@ -33,7 +33,7 @@ elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST["addToBasket"]) && $_POST["addToBasket"] === "addToBasket") {
         $day = $_POST["day"];
         $amount = $_POST["amount"];
-        if (isset($day) && !empty($day) && isset($amount) && !empty($amount) && is_numeric($amount) && $amount <= $storage->getAvailableTicketsByDay($day)) {
+        if (isset($day) && !empty($day) && isset($amount) && is_numeric($amount) && $amount <= $storage->getAvailableTicketsByDay($day)) {
 
             if (!isset($_SESSION["basket"])) {
                 $basket = array();
@@ -51,5 +51,37 @@ elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
             unset($_SESSION["basket"][$_POST["deleteFromBasket"]]);
         }
         header("Location:/tickets.php");
+    }
+    elseif (isset($_POST["payForBasket"]) && $_POST["payForBasket"] === "payForBasket") {
+        if (isset($_POST["paymentOption"]) && !empty($_POST["paymentOption"])) {
+            if (isset($_SESSION["basket"]) && ! empty($_SESSION["basket"])) {
+                $basket = $_SESSION["basket"];
+                //calculate total serverside
+                $total = 0;
+                foreach ($basket as $day => $amount) {
+                    $total += $priceList[$day] * $amount;
+                }
+                if ($total > 0) {
+                    if ($transactionId = $paymentEngine->executePayment($_POST["paymentOption"], $total)) {
+                        $reservationSuccess = true;
+                        foreach ($basket as $day => $amount) {
+                            if ($reservationSuccess == true && $amount <= $storage->getAvailableTicketsByDay($day)) {
+                                $reservationSuccess = $storage->createOrUpdateReservation($user->id, $day, $amount);
+                            }
+                            else {
+                                $reservationSuccess = false;
+                            }
+                        }
+                        if ($reservationSuccess == true) {
+                            unset($_SESSION["basket"]);
+                            //header("Location:/payment.php");
+                        }
+                        else {
+                            $pBuilder->buildErrorPage("Uw bestelling kon niet correct uitgevoerd worden, neem contact op met een administrator");
+                        }
+                    }
+                }
+            }
+        }
     }
 }

48854c822e2abe5f856b658e1ebe2217d0f42707
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-06-07 18:45:59 +0200

    Payment redirect page

---
+++ payment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaumevandecasteele
+ * Date: 07/06/16
+ * Time: 17:46
+ */
+include "php/PageBuilder.php";
+$pBuilder = new PageBuilder();
+header("refresh:5; URL=/tickets.php");
+$pBuilder->addHead("Betaling");
+$pBuilder->addNavBar(null);
+$pBuilder->addError("U wordt nu doorverwezen naar uw betalingsinstelling. Bij het succesvol uitvoeren van de betaling wordt u teruggebracht naar een overzicht van uw reservaties.");
+$pBuilder->addFooter();
